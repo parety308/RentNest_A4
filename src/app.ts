7b729bf2023629fab8ef6a712102077a0848fad9
MD Parvez Hasan <md.parvez.hasan.dev@gmail.com>
2026-07-05 22:47:06 +0600

    added after initialize express

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,18 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
-import HttpsStatus from "http-status-codes"
+import HttpsStatus from "http-status-codes";
+import cors from "cors"
+import { config } from './config';
 const app = express()
 
+//use middleware
+app.use(cors({
+    origin: config.app_url,
+    credentials: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 function startServer() {
     app.get('/', (req, res) => {
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import app from "./app";
 import { config } from "./config";
+import { prisma } from "./lib/prisma";
 
-function main() {
-    app.listen(config.port, () => {
-        console.log(`RentNest app listening on port ${config.port}`)
-    });
+async function main() {
+    try {
+        await prisma.$connect();
+        console.log("Database Connected Successfully")
+        app.listen(config.port, () => {
+            console.log(`RentNest app listening on port ${config.port}`)
+        });
+    } catch (error) {
+        await prisma.$disconnect();
+        console.log(error);
+        process.exit(1);
+    }
 }
 
 main();
