6d6ef868534d54097100693b5746a2e38a02a966
MD Parvez Hasan <md.parvez.hasan.dev@gmail.com>
2026-07-11 20:12:17 +0600

    fix express server start on render

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,13 @@ import { prisma } from "./lib/prisma";
 async function main() {
     try {
         await prisma.$connect();
-        console.log("Database Connected Successfully")
-        // app.listen(config.port, () => {
-        //     console.log(`RentNest app listening on port ${config.port}`)
-        // });
+
+        console.log("Database Connected Successfully");
+
+        app.listen(config.port, () => {
+            console.log(`RentNest app listening on port ${config.port}`);
+        });
+
     } catch (error) {
         await prisma.$disconnect();
         console.log(error);
